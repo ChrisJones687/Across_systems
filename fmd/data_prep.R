045975c5dd826c39caf3c528ea34532b89fadd2f
Chris Michael Jones <cmjone25@wolftech.ad.ncsu.edu>
2019-09-06 20:04:07 -0400

    added merge for cattle to CO counties

--- fmd/data_prep.R
+++ fmd/data_prep.R
@@ -1,33 +1,40 @@
 library(raster)
 library(sp)
 library(rgdal)
-fmd <- read.csv("/Users/rachellantz/Google Drive File Stream/My Drive/EEID/Foot and Mouth Disease/FMD dummy.csv")
+fmd <- read.csv("H:/My Drive/EEID/Foot and Mouth Disease/FMD dummy.csv")
 
-nlcd2016 <- raster("/Users/rachellantz/Google Drive File Stream/My Drive/EEID/Foot and Mouth Disease/NLCD_2016_Land_Cover_L48_20190424.img")
-states <- readOGR("/Users/rachellantz/Google Drive File Stream/My Drive/EEID/Foot and Mouth Disease/us_lower_48_states.shp")
+nlcd2016 <- raster("H:/My Drive/EEID/Foot and Mouth Disease/NLCD_2016_Land_Cover_L48_20190424.img")
+states <- readOGR("H:/My Drive/EEID/Foot and Mouth Disease/us_lower_48_states.shp")
 texas <- states[states$STATE_NAME == 'Texas',]
 texas <- spTransform(texas, CRSobj = crs(nlcd2016))
 
 texas_nlcd <- crop(nlcd2016, texas)
 texas_nlcd <- mask(texas_nlcd, texas)
 texas_nlcd[texas_nlcd != c(81,71)] <-0
-writeRaster(texas_nlcd81, "/Users/rachellantz/Google Drive File Stream/My Drive/EEID/Foot and Mouth Disease/texas_pasture.tif")
+writeRaster(texas_nlcd81, "H:/My Drive/EEID/Foot and Mouth Disease/texas_pasture.tif")
 
 ### Colorado
-nlcd2016 <- raster("/Users/rachellantz/Google Drive File Stream/My Drive/EEID/Foot and Mouth Disease/NLCD_2016_Land_Cover_L48_20190424.img")
-states <- readOGR("/Users/rachellantz/Google Drive File Stream/My Drive/EEID/Foot and Mouth Disease/us_lower_48_states.shp")
+nlcd2016 <- raster("H:/My Drive/EEID/Foot and Mouth Disease/NLCD_2016_Land_Cover_L48_20190424.img")
+states <- readOGR("H:/My Drive/EEID/Foot and Mouth Disease/us_lower_48_states.shp")
 colorado <- states[states$STATE_NAME == 'Colorado',]
 colorado <- spTransform(colorado, CRSobj = crs(nlcd2016))
 
 colorado_nlcd <- crop(nlcd2016, colorado)
 colorado_nlcd <- mask(colorado_nlcd, colorado)
-colorado_nlcd[colorado_nlcd != c(81,71)] <-0
-writeRaster(colorado_nlcd81, "/Users/rachellantz/Google Drive File Stream/My Drive/EEID/Foot and Mouth Disease/colorado_pasture.tif")
+colorado_nlcd[colorado_nlcd == 81] <- 1
+colorado_nlcd[colorado_nlcd == 71] <- 1
+colorado_nlcd[colorado_nlcd > 1] <- 0
+
+writeRaster(colorado_nlcd, "H:/My Drive/EEID/Foot and Mouth Disease/colorado_pasture.tif")
+colorado_nlcd_300m <- aggregate(colorado_nlcd, fact = 10, fun = 'sum')
+writeRaster(colorado_nlcd_300m, "H:/My Drive/EEID/Foot and Mouth Disease/colorado_pasture_300m.tif")
 
 ### Colorado counties
-counties <- readOGR("/Users/rachellantz/Google Drive File Stream/My Drive/EEID/Foot and Mouth Disease/COUNTY DATA.shp")
-COcounties <- counties[counties$STATE_NAME == 'Colorado']
-
-COcounties_pasture <- extract(colorado_nlcd300m, COcounties, fun = 'sum')
-colorado_cattle <- read.csv("/Users/rachellantz/Google Drive File Stream/My Drive/EEID/Foot and Mouth Disease/colorado.csv")
-
+counties <- readOGR("H:/My Drive/EEID/Foot and Mouth Disease/us_lower_48_counties.shp")
+COcounties <- counties[counties$STATE_NAME == 'Colorado',]
+COcounties <- spTransform(COcounties, CRSobj = crs(colorado_nlcd_300m))
+COcounties$area_of_pasture <- extract(colorado_nlcd_300m, COcounties, fun = sum)
+# COcounties_pasture <- extract(colorado_nlcd_300m, COcounties, fun = sum)
+colorado_cattle <- read.csv("H:/My Drive/EEID/Foot and Mouth Disease/colorado.csv")
+names(colorado_cattle)[1] <- "NAME"
+COcounties <- merge(COcounties, colorado_cattle, by = 'NAME')
