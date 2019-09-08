c6308b5dac6c4583b9b2f3ec02ee25e7376f4430
Bhim Chaulagain <agribhimchaulagain@gmail.com>
2019-09-08 11:30:49 -0700

    Treatments added

--- installpackages.R
+++
@@ -1,4 +0,0 @@
-install.packages("raster")
-library(raster)
-install.packages("devtools")
-devtools::install_github("https://github.com/ncsu-landscape-dynamics/rpops")
---
+++ wheat stripe rust/installpackages.R
@@ -0,0 +1,4 @@
+install.packages("raster")
+library(raster)
+install.packages("devtools")
+devtools::install_github("https://github.com/ncsu-landscape-dynamics/rpops")
