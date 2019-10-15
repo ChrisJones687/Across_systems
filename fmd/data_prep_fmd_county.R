9863cd9b360dd94f62493253d1021579e3799f48
Lantz <lantzra@oregonstate.edu>
2019-10-14 17:01:00 -0700

    distribute cattle by county across pasture area in VA

--- fmd/data_prep_fmd_county.R
+++ fmd/data_prep_fmd_county.R
@@ -1,36 +1,55 @@
-### Aggregate zip code cattle to county level for FMD
-### Colorado (counties)
+### Evenly distribute cattle in pasture by counties for FMD
+library(raster)
+library(rgdal)
+## Virginia (counties)
+# Read in data files
 counties <- readOGR("G:/My Drive/EEID/Foot and Mouth Disease/us_lower_48_counties.shp")
 VA_counties <- counties[counties$STATE_NAME == 'Virginia',]
 virginia_nlcd_3000m <- raster("G:/My Drive/EEID/Foot and Mouth Disease/states/VA_nlcd_3000m.img")
 VA_counties <- spTransform(VA_counties, CRSobj = crs(virginia_nlcd_3000m))
-VA_counties$area_of_pasture <- extract(virginia_nlcd_3000m, VA_counties, fun = sum)
-# COcounties_pasture <- extract(colorado_nlcd_300m, COcounties, fun = sum)
-writeOGR(COcounties, "G:/My Drive/EEID/Foot and Mouth Disease/virginia_counties_pasturecattle.shp")
 virginia_cattle <- read.csv("G:/My Drive/EEID/Foot and Mouth Disease/virginia_county_cattle.csv")
+
+# Organize and reorder data
 names(virginia_cattle)[2] <- "NAME"
 VA_counties <- VA_counties[order(VA_counties$NAME),]
 VA_counties$cattle <- virginia_cattle$cattle
+VA_counties$area <- virginia_cattle$area
+VA_counties$cattle_per_area <- virginia_cattle$cattle_per_area
 VA_counties$NAME <- as.character(VA_counties$NAME)
 virginia_cattle$NAME <- as.character(virginia_cattle$NAME)
-VA_counties2 <- left_join(VA_counties@data, virginia_cattle, by = 'NAME')
+VA_counties$area <- as.numeric(VA_counties$area)
 
-# Distribute cattle across the county area
-VA_counties$cattle <- as.numeric(VA_counties$cattle)
-VA_counties$area_of_pasture <- as.numeric(VA_counties$area_of_pasture)
-VA_counties$cattleperarea <- VA_counties$cattle/VA_counties$area_of_pasture
-# Average number of cattle in 30 x 30 m of pasture/grassland in the county
-VA_counties$cattleperarea
-plot(virginia_nlcd_3000m)
-plot(VA_counties, add = TRUE)
-
-# Extract the values for the number of cattle for each county
-for (i in 1:64) {
+# Assign cattle for each raster cell based on county data
+cell_num <- Which(virginia_nlcd_3000m==0, cells = TRUE)
+cell_num <- as.numeric(cell_num)
+for (i in 1:134) {
   ex <- extract(virginia_nlcd_3000m, VA_counties[i,], cellnumbers = TRUE)
   ex <- data.frame(ex)
   ex <- ex$cell
-  virginia_nlcd_300m[ex] <- ceiling(virginia_nlcd_300m[ex]*VA_counties$cattleperarea[i]*100)
+  virginia_nlcd_3000m[ex] <- ceiling(virginia_nlcd_3000m[ex]*VA_counties$cattle_per_area[i])
 }
-colorado_nlcd_300m[colorado_nlcd_300m==0] <- NA
-colorado_nlcd_300m <- mask(colorado_nlcd_300m, colorado)
-plot(colorado_nlcd_300m)
+states <- readOGR("G:/My Drive/EEID/Foot and Mouth Disease/us_lower_48_states.shp")
+virginia <- states[states$STATE_NAME == 'Virginia',]
+virginia <- spTransform(virginia, CRSobj = crs(virginia_nlcd_3000m))
+virginia_nlcd_3000m <- mask(virginia_nlcd_3000m, virginia)
+writeRaster(virginia_nlcd_3000m, "C:/Users/lantzra/Desktop/VA_nlcd_3000m_cattle.img", overwrite = TRUE)
+# Update raster so the cells without pasture have zero cattle
+VA_nlcd_3000m_cattle <- raster("C:/Users/lantzra/Desktop/VA_nlcd_3000m_cattle.img")
+update(VA_nlcd_3000m_cattle, v=rep(0, 18923), cell=cell_num)
+VA_nlcd_3000m_cattle <- mask(VA_nlcd_3000m_cattle, virginia)
+writeRaster(VA_nlcd_3000m_cattle, "G:/My Drive/EEID/Foot and Mouth Disease/states/VA_nlcd_3000m_cattle.img", overwrite = TRUE)
+VA_nlcd_3000m_cattle <- raster("G:/My Drive/EEID/Foot and Mouth Disease/states/VA_nlcd_3000m_cattle.img")
+
+# Write out host, total hosts, and infected files
+writeRaster(VA_nlcd_3000m_cattle, "G:/My Drive/EEID/Foot and Mouth Disease/states/virginia/host.tif", overwrite = TRUE)
+VA_nlcd_3000m_cattle[VA_nlcd_3000m_cattle >=0] <- maxValue(VA_nlcd_3000m_cattle)
+writeRaster(VA_nlcd_3000m_cattle, "G:/My Drive/EEID/Foot and Mouth Disease/states/virginia/total_hosts.tif", overwrite = TRUE)
+host <- raster("G:/My Drive/EEID/Foot and Mouth Disease/states/virginia/host.tif")
+host[host==2120]
+infected <- host
+infected[infected<2120] <- 0
+infected[infected==2120] <- 1
+infected[infected==0] <- NA
+infected[infected==1]
+plot(infected)
+writeRaster(infected, "G:/My Drive/EEID/Foot and Mouth Disease/states/virginia/infected.tif")
